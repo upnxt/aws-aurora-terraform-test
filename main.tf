5d495b838b37d9e0929d1b362646385b2e10e14b
upnxt <upnxt@users.noreply.github.com>
2022-04-19 17:08:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,75 +1,19 @@
-locals {
-  name   = "aurora-test1"
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-}
-
-################################################################################
-# Supporting Resources
-################################################################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = local.name
-  cidr = "10.99.0.0/18"
-
-  azs              = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
-  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
-
-  tags = local.tags
-}
-
-################################################################################
-# RDS Aurora Module - PostgreSQL
-################################################################################
-
-module "aurora_postgresql" {
-  source = "../../"
-
-  name              = "${local.name}-postgresql"
-  engine            = "aurora-postgresql"
-  engine_mode       = "serverless"
-  storage_encrypted = true
-
-  vpc_id                = module.vpc.vpc_id
-  subnets               = module.vpc.database_subnets
-  create_security_group = true
-  allowed_cidr_blocks   = module.vpc.private_subnets_cidr_blocks
-
-  monitoring_interval = 60
-
-  apply_immediately   = true
-  skip_final_snapshot = true
-
-  db_parameter_group_name         = aws_db_parameter_group.example_postgresql.id
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.example_postgresql.id
-  # enabled_cloudwatch_logs_exports = # NOT SUPPORTED
-
-  scaling_configuration = {
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-mysql"  
+  engine_mode             = "serverless"  
+  database_name           = "myauroradb"  
+  enable_http_endpoint    = true  
+  master_username         = "sa"
+  master_password         = "232-3sdcrb34gh134rrtn6uk89k78j5h"
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  
+  scaling_configuration {
     auto_pause               = true
-    min_capacity             = 2
-    max_capacity             = 16
+    min_capacity             = 1    
+    max_capacity             = 1
     seconds_until_auto_pause = 300
     timeout_action           = "ForceApplyCapacityChange"
-  }
-}
-
-resource "aws_db_parameter_group" "example_postgresql" {
-  name        = "${local.name}-aurora-db-postgres-parameter-group"
-  family      = "aurora-postgresql10"
-  description = "${local.name}-aurora-db-postgres-parameter-group"
-  tags        = local.tags
-}
-
-resource "aws_rds_cluster_parameter_group" "example_postgresql" {
-  name        = "${local.name}-aurora-postgres-cluster-parameter-group"
-  family      = "aurora-postgresql10"
-  description = "${local.name}-aurora-postgres-cluster-parameter-group"
-  tags        = local.tags
+  }  
 }
