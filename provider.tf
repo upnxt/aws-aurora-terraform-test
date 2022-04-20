d58430fe2aabd62375aefff4d54d4529b4f8a828
upnxt <upnxt@users.noreply.github.com>
2022-04-20 08:58:22 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,10 @@ provider "aws" {
   region = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  tags = {
-    Environment = "dev"
+  default_tags {
+    tags = {
+      Environment = "dev"
+      Project     = "sTest1"   
+    }
   }
 }
